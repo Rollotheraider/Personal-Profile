a01ed520f7980b14f869acd0c1ef686b633de7c1
Tony Infant P <92947343+Rollotheraider@users.noreply.github.com>
2025-02-16 21:48:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     const links = document.querySelectorAll("nav ul li a");
-    
+
     links.forEach(link => {
         link.addEventListener("click", function(event) {
             event.preventDefault();
             const sectionId = this.getAttribute("href").substring(1);
-            document.getElementById(sectionId).scrollIntoView({
-                behavior: "smooth"
-            });
+            const section = document.getElementById(sectionId);
+            
+            if (section) {
+                section.scrollIntoView({ behavior: "smooth" });
+            }
         });
     });
 });
